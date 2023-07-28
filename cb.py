9da8061a83698a59e7a2dcec45f36afa64eec6db
Andrew Falgout <andrew.falgout@gmail.com>
2023-07-27 19:37:10 -0500

    Added ability to accept a file from STDIN

--- cb.py
+++ cb.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 # filename: cb.py
-# author: draeician (July 22, 2023)
+# author: draeician (July 27, 2023)
 # purpose: allow for files to be placed in the system clipboard quickly and easily
 
 import argparse
 import subprocess
 import os
+import sys
 import pyperclip
 
 def copy_file_contents_to_clipboard(file_paths, include_header=False):
@@ -24,25 +25,30 @@ def copy_file_contents_to_clipboard(file_paths, include_header=False):
         all_file_contents = ""
 
         for file_path in file_paths:
-            # Get the absolute path of the file
-            file_path = os.path.abspath(file_path)
+            file_contents = ""
+            if file_path == '-':
+                for line in sys.stdin:
+                    file_contents += line
+                file_path = "STDIN"
+            else:
+                # Get the absolute path of the file
+                file_path = os.path.abspath(file_path)
 
-            # Verify if the file exists
-            try:
-                with open(file_path, 'r') as file:
-                    file_contents = file.read().strip()
+                # Verify if the file exists
+                try:
+                    with open(file_path, 'r') as file:
+                        file_contents = file.read().strip()
+                except FileNotFoundError:
+                    print(f"Error: File '{file_path}' not found.")
+                    copied_all_files = False
 
-                # Include header if specified
-                if include_header:
-                    header = f"=== File: {file_path} ===\n"
-                    file_contents = header + file_contents
+            # Include header if specified
+            if include_header:
+                header = f"=== File: {file_path} ===\n"
+                file_contents = header + file_contents
 
-                # Add the file contents to the combined string
-                all_file_contents += file_contents + "\n\n"
-
-            except FileNotFoundError:
-                print(f"Error: File '{file_path}' not found.")
-                copied_all_files = False
+            # Add the file contents to the combined string
+            all_file_contents += file_contents + "\n\n"
 
         # Use pyperclip to copy the combined file contents
         pyperclip.copy(all_file_contents)
