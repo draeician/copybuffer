668896f61d839b96273fe55d1a56f0b4440a8a73
Andrew Falgout <andrew.falgout@gmail.com>
2023-08-09 11:32:14 -0500

    Added image support to Clipboard Manager, including handling of animated GIFs

--- README.md
+++ README.md
@@ -1,17 +1,18 @@
 # Clipboard Manager (cb.py)
 
 ## Description
-Clipboard Manager is a simple Python script that allows you to quickly and easily copy the contents of one or more files to the system clipboard. This script makes use of the `pyperclip` library, which provides cross-platform clipboard access.
+Clipboard Manager is a versatile Python script that allows you to quickly and easily copy the contents of one or more files or images to the system clipboard. In addition to text files, it now supports various image formats, including PNG, JPEG, BMP, and GIF. For animated GIFs, only the first frame is copied to the clipboard. This script makes use of the `pyperclip` library and `xclip`, providing cross-platform clipboard access.
 
 ## Requirements
 - Python 3.x
 - `pyperclip` library
+- `xclip` (for handling images on Linux)
 
 ## Usage
+
+### Copying Text Files
 1. Clone this repository or download the `cb.py` script to your local machine.
-
 2. Open a terminal or command prompt in the directory containing `cb.py`.
-
 3. To copy the contents of one or more files to the clipboard, use the following command:
 
     ```bash
@@ -23,31 +24,13 @@ Clipboard Manager is a simple Python script that allows you to quickly and easil
 
 4. The contents of the specified file(s) will be copied to the system clipboard. If multiple file paths are provided, the script will copy the contents of each file in sequence.
 
-5. After running the command, you can paste the clipboard contents into any application that supports pasting.
+### Copying Images
+1. To copy an image to the clipboard, use the same command as for text files, providing the image path:
 
-## Example
-To copy the contents of two files, `file1.txt` and `file2.txt`, to the clipboard and include headers for each file, use the following command:
+    ```bash
+    python cb.py <image_path>
+    ```
 
-```bash
+    - Supported image formats: PNG, JPEG, BMP, GIF (first frame only for animated GIFs)
 
-python cb.py --header file1.txt file2.txt
-
-
-This will copy the contents of file1.txt and file2.txt to the clipboard, including headers if the --header flag is used.
-
-Additional Notes
-The cb.py script has been updated to support multiple file paths as arguments, allowing you to copy the contents of several files to the clipboard at once.
-
-The script now includes an option to include a header with the filename before each file's contents when copying to the clipboard.
-
-The requirements.txt file has been added to specify the required pyperclip library for the script. Install it using pip before running the script.
-
-Feel free to modify the script according to your needs or contribute to its development by submitting pull requests.
-
-License
-This project is licensed under the MIT License - see the LICENSE file for details.
-
-Author
-draeician
-Acknowledgments
-Special thanks to the developers of the pyperclip library for providing a simple cross-platform clipboard access solution.
+2. The image will be copied to the system clipboard, ready to be pasted into other applications.
--- cb.py
+++ cb.py
@@ -1,29 +1,51 @@
 #!/usr/bin/env python3
-# filename: cb.py
-# author: draeician (July 27, 2023)
-# purpose: allow for files to be placed in the system clipboard quickly and easily
 
 import argparse
-import subprocess
+from PIL import Image
+import pyperclip
 import os
 import sys
-import pyperclip
+import tempfile
+import subprocess
+import shutil
+
+def is_xclip_installed():
+    return shutil.which("xclip") is not None
+
+def copy_image_to_clipboard(image_path):
+    if not is_xclip_installed():
+        print("Error: xclip is not installed. Please install it using 'sudo apt-get install xclip'.")
+        return False
+
+    try:
+        # Open the image
+        image = Image.open(image_path)
+
+        # If it's a GIF, take the first frame and convert to RGB
+        if image_path.lower().endswith('.gif'):
+            image = image.convert('RGB')
+
+        # Create a temporary file to hold the image
+        temp_file = tempfile.NamedTemporaryFile(delete=False, suffix='.png')
+        temp_path = temp_file.name
+
+        # Save the image to the temporary file
+        image.save(temp_path)
+
+        # Use xclip to copy the image to the clipboard
+        command = f"xclip -selection clipboard -t image/png -i {temp_path}"
+        subprocess.run(command, shell=True)
+
+        return True
+    except Exception as e:
+        print(f"Error: An unexpected error occurred while copying the image. {str(e)}")
+        return False
+
 
 def copy_file_contents_to_clipboard(file_paths, include_header=False):
-    """
-    Copies the contents of files to the clipboard using the pyperclip library.
-
-    Args:
-        file_paths (list): List of file paths to copy.
-        include_header (bool): Flag to indicate whether to include a header with the filename.
-
-    Returns:
-        bool: True if all files exist and their contents are copied successfully, False otherwise.
-    """
     try:
         copied_all_files = True
         all_file_contents = ""
-
         for file_path in file_paths:
             file_contents = ""
             if file_path == '-':
@@ -31,10 +53,7 @@ def copy_file_contents_to_clipboard(file_paths, include_header=False):
                     file_contents += line
                 file_path = "STDIN"
             else:
-                # Get the absolute path of the file
                 file_path = os.path.abspath(file_path)
-
-                # Verify if the file exists
                 try:
                     with open(file_path, 'r') as file:
                         file_contents = file.read().strip()
@@ -42,37 +61,36 @@ def copy_file_contents_to_clipboard(file_paths, include_header=False):
                     print(f"Error: File '{file_path}' not found.")
                     copied_all_files = False
 
-            # Include header if specified
             if include_header:
-                header = f"=== File: {file_path} ===\n"
+                header = f"=== File: {file_path} ===\\n"
                 file_contents = header + file_contents
 
-            # Add the file contents to the combined string
-            all_file_contents += file_contents + "\n\n"
+            all_file_contents += file_contents + "\\n\\n"
 
-        # Use pyperclip to copy the combined file contents
         pyperclip.copy(all_file_contents)
-
         return copied_all_files
     except Exception as e:
         print(f"Error: An unexpected error occurred. {str(e)}")
         return False
 
 def main():
-    # define debug mode
     debug = False
-
-    parser = argparse.ArgumentParser(description="Copy file contents to clipboard.")
-    parser.add_argument("file_paths", metavar='N', nargs="+", help="Paths of the files to copy.")
-    parser.add_argument("--header", action="store_true", help="Include header.")
+    parser = argparse.ArgumentParser(description="Copy file contents or images to clipboard.")
+    parser.add_argument("file_paths", metavar='N', nargs="+", help="Paths of the files or images to copy.")
+    parser.add_argument("--header", action="store_true", help="Include header for text files.")
     args = parser.parse_args()
 
     if debug:
         print(args.file_paths)
 
-    copy_successful = copy_file_contents_to_clipboard(args.file_paths, args.header)
-    if copy_successful:
-        print("File contents copied to the clipboard successfully!")
+    for file_path in args.file_paths:
+        if file_path.lower().endswith(('.png', '.jpg', '.jpeg', '.bmp', '.gif')):
+            copy_successful = copy_image_to_clipboard(file_path)
+        else:
+            copy_successful = copy_file_contents_to_clipboard(args.file_paths, args.header)
+
+        if copy_successful:
+            print(f"{file_path} copied to the clipboard successfully!")
 
 if __name__ == '__main__':
     main()
