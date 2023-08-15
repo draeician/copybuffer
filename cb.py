8c84c5618505a4d8e089b0fca1cf6951788c5708
Andrew Falgout <andrew.falgout@gmail.com>
2023-08-15 17:31:40 -0500

    updated to 1.0.4

--- cb.py
+++ cb.py
@@ -9,7 +9,7 @@ import tempfile
 import subprocess
 import shutil
 
-__VERSION__ = "v1.0-3-g423b6c9"
+__VERSION__ = "v1.0-4-ged68b6e"
 
 def is_xclip_installed():
     return shutil.which("xclip") is not None
